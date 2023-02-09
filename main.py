4dbe508e85d5db3611bf65110be5363e21491a27
bossa96demo <ilyaershov09@gmail.com>
2023-02-09 18:48:26 +0300

    changed code, removed unnecessary code

--- main.py
+++ main.py
@@ -1,72 +1,42 @@
 """
 Simple Bot to change description for today's date
 """
+
 from typing import Optional, Tuple
-
-from telegram import __version__ as TG_VER
-
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)
 from telegram import Chat, ChatMember, ChatMemberUpdated, Update
 from telegram.constants import ParseMode
-from telegram.ext import (
-    Application,
-    ChatMemberHandler,
-    CommandHandler,
-    ContextTypes,
-    MessageHandler,
-    filters,
-)
+from telegram.ext import (Application, ChatMemberHandler, CommandHandler, ContextTypes, MessageHandler, filters)
 import calendar
 import datetime
 
-async def greet_chat_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Greets new users in chats and announces when someone leaves"""
-    result = extract_status_change(update.chat_member)
-    if result is None:
-        return
-
-    was_member, is_member = result
-    member_name = update.chat_member.new_chat_member.user.mention_html()
-
-    if not was_member and is_member:
-        await update.effective_chat.send_message(
-            f"{member_name} joined us!",
-            parse_mode=ParseMode.HTML,
-        )
-    elif was_member and not is_member:
-        await update.effective_chat.send_message(
-            f"{member_name} left us.",
-            parse_mode=ParseMode.HTML,
-        )
-
-
-
+# Function to change description
 async def change_description(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Setting ID
     channel_id = "CHAT'S ID OR CHANNEL NICKNAME HERE"
+    # Getting today's date
     today = datetime.date.today()
+    # Making it human-readable
     date_in_words = str(today.day) + ' ' + calendar.month_name[today.month]
+    # You can change it
     description = 'Happy ' + date_in_words + '!!🎉'
+    # Updating chat description
     await context.bot.setChatDescription(channel_id, description)
-    print("\ndescription successfully changed\n")
+    # Printing success message
+    print("\n*Description successfully updated*\n")
 
 def main() -> None:
-    print("bot was started\n\n")
+    print("*The bot was started*\n\n")
     """Start the bot."""
+
     # Create the Application and pass it your bot's token.
     application = Application.builder().token("YOUR TOKEN HERE").build()
 
-    #set command for bot to change desc
+    # Set command for bot to change desc
     application.add_handler(CommandHandler("run", change_description))
-
-    # Handle members joining/leaving chats.
-    application.add_handler(ChatMemberHandler(greet_chat_members, ChatMemberHandler.CHAT_MEMBER))
 
     # Run the bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-
+# Calling main 
 if __name__ == "__main__":
     main()
